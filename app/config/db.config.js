cb4188a33a75218b200b60738045efb206f0c1d2
Antoine <amilienantoine.dev@gmail.com>
2020-12-21 14:35:02 +0100

    Intial commit - BackEnd exemple. NodeJs Express MangoDd

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: "mongodb+srv://root:toor@cluster1.2olg5.mongodb.net/MaBelleBdd?retryWrites=true&w=majority"
+};
+
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+//connexion a mangoDb
+const db = require("./app/models");
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+//USING
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+// Declaration du fichier avec les routes personalisées pour le controller tutoriel
+require("./app/routes/tutorial.routes")(app);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
